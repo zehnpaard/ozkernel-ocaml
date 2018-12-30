67fb1406e3eeb83570f84e8afbddce48997e5379
zehnpaard <zehnpaard@googlemail.com>
2018-12-31 00:57:09 +0900

    Add runOneStep for Declare

--- 01_min/abstract_machine.ml
+++ 01_min/abstract_machine.ml
@@ -29,11 +29,15 @@ let stack : (Ast.t, Env.t) list ref = ref []
 
 let runOneStep statement env = match statement with
   | Skip -> ()
-  | Seq (s1, s2) ->
-      stack := (s1, env) :: (s2, env) :: !stack
+  | Seq (s1, s2) -> stack := (s1, env) :: (s2, env) :: !stack
   | VarBind (x1, x2) -> ()
   | ValBind (x, v) -> ()
-  | Declare (x, s) -> ()
+  | Declare (x, s) ->
+      begin
+        i = makeVar ();
+        env' = Env.add x i env;
+        stack := (s, env') :: !stack
+      end
   | ProcCall (x, args) -> ()
 
 let run = match !stack with
