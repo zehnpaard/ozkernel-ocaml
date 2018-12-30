8fe9bbd2e9d6f6f01c280c04cae4060c0ef0eda2
zehnpaard <zehnpaard@googlemail.com>
2018-12-30 23:25:34 +0900

    Implement store for abstract machine

---
+++ 01_min/abstract_machine.ml
@@ -0,0 +1,23 @@
+type t =
+  | Var of int
+  | Int of int
+
+let store = Array.make 100000 (Int 0)
+
+let f = ref 0
+
+let makeVar () =
+  let i = !f in
+  begin
+    store.(i) <- Var (-1);
+    f := i + 1;
+    i
+  end
+
+let makeInt n
+  let i = !f in
+  begin
+    store.(i) <- Int n;
+    f := i + 1;
+    i
+  end
