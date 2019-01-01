6b5ead5c9361fff4726b057a6b6a8ff1b54c9d60
zehnpaard <zehnpaard@googlemail.com>
2019-01-02 07:45:48 +0900

    Add support for creating and calling procedures

--- 02_proc/abstract_machine.ml
+++ 02_proc/abstract_machine.ml
@@ -1,26 +1,28 @@
+(* string -> int *)
+module Env = Map.Make(String)
+
 type t =
   | Var of int
   | Int of int
+  | Proc of Ast.v * Env.t
 
 let store = Array.make 100000 (Int 0)
 
 let f = ref 0
 
-let makeVar () =
+let makeInStore v =
   let i = !f in
   begin
-    store.(i) <- Var (-1);
+    store.(i) <- v;
     f := i + 1;
     i
   end
 
-let makeInt n =
-  let i = !f in
-  begin
-    store.(i) <- Int n;
-    f := i + 1;
-    i
-  end
+let makeVar () = makeInStore (Var (-1))
+let makeInt n = makeInStore (Int n)
+let makeProc v e = match v with
+  | Ast.Proc _ -> makeInStore (Proc (v, e)) (* Inefficient closure, must fix *)
+  | _ -> failwith "makeProc called with non-procedure value"
 
 let getParent i = match store.(i) with
   | Var j when not j = (-1) ->
@@ -30,9 +32,6 @@ let getParent i = match store.(i) with
 
 let bind ~parent ~child =
   store.(child) := Var !parent
-
-(* string -> int *)
-module Env = Map.Make(string)
 
 let stack : (Ast.t, Env.t) list ref = ref []
 
@@ -49,8 +48,22 @@ let intBinOp f i j k =
      | (Int n1, Int n2) ->
          let a = makeInt (f n1 n2) in
          store.(k) := Var a
-     | (Var i, _) | (_, Var i) -> UnboundVariable i
+     | (Var i, _) | (_, Var i) -> raise (UnboundVariable i)
      | _ -> failwith "Non-int value passed to int binOp"
+
+let callProcedureFromEnv env x args =
+  let p = Env.find x env in
+  match p with
+    | Var i -> raise (UnboundVariable i)
+    | Proc (Ast.Proc (params, s), e) ->
+        if not (List.length args = List.length params) then
+          failwith "Invalid number of arguments passed to procedure"
+        else
+          let f k a b = Some a in
+          let env1 = Env.union f e env in
+          let env2 = List.fold_right2 Env.add params args env1 in
+          stack := (s, env2) :: !stack
+    | _ -> failwith "Non-procedure in position of called procedure"
 
 let procCall env x args =
   let args' = getArgs env args in
@@ -60,6 +73,7 @@ let procCall env x args =
     | ("*", [i; j; k]) -> intBinOp ( * ) i j k
     | ("div", [i; j; k]) -> intBinOp (/) i j k
     | ("mod", [i; j; k]) -> intBinOp mod i j k
+    | _ when Env.mem x env -> callProcedureFromEnv env x args
     | _ -> failwith "Unknown procedure"
 
 let runOneStep statement env = match statement with
